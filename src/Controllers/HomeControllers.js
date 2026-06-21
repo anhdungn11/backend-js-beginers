80c5b1f6fbfdce7ec7d97c26523ab881fe7d9931
nanhdung11 <nanhdung840@gmail.com>
2026-06-21 21:22:00 +0700

    connect sql

--- src/Controllers/HomeControllers.js
+++ src/Controllers/HomeControllers.js
@@ -12,9 +12,36 @@ const getTrang2=(req,res)=>{
     res.send('check thu trang 2'); // Gửi phản hồi "check thu" khi truy cập vào trang tiếp theo
 }
 const postCreateUser=(req,res)=>{
-    console.log(">>>>req.body :",req.body) // lay ra thong tin nguoi dung
-res.send('create new user') // tra ra ket qua cho nguoi dung 
+    // console.log(">>>>req.body :",req.body) //lay ra thong tin nguoi dung
+
+    //cach 1 lay ra thon tin mguoi dung
+     let email = req.body.email;  
+    let name = req.body.name;  
+    let city = req.body.city;
+
+    // cach 2 de lay ra thong tin
+    // let {email,name,city} = req.body
+
+    console.log("email=",email ,"name=" , name , "city= ", city )
+
+ 
+
+connection.query(   // ket noi voi csdl luu truc tiep tu client
+`INSERT INTO Users (email,name,city) 
+ VALUES (?,?,?)`,
+ [email,name,city],
+ function(err,results){
+    console.log(results);
+
+    res.send('created success')   // tra ra ket qua cho nguoi dung
+ }
+)
 }
+
+//  
+
+
+
 module.exports={
      getHomePage:getHomePage,
     getTrangTiep:getTrangTiep,
